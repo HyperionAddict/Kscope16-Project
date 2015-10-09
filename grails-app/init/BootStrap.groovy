d4e617340ea0f20494f90a3a21e2ccbb3198c96e
cube-head <joe.aultman@gmail.com>
2015-10-09 18:27:14 -0400

    Remove debug code from Bootstrap.groovy

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -9,15 +9,10 @@ class BootStrap {
         svr.name = 'sandboxcv'
 
         def essHome = IEssbase.Home.create(IEssbase.JAPI_VERSION)
-        println 'JAPI found.'
 
-        def tst = new File('grails-app/init/test.txt').canonicalPath
-        println tst
         def pw = new File('grails-app/init/pw.txt').text.trim()
-        println ":::${pw}:::"
 
         def essSvr = essHome.signOn('jaultman', pw, false, null, 'embedded', svr.name)
-        println 'Embedded mode enabled.'
 
         svr.apps = ''
         essSvr.applications.all.each {
